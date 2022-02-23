d50e6d62da4f3f7ed2e832e13d832b9665dba707
catalinpit91 <92463435+catalinpit91@users.noreply.github.com>
2022-02-23 09:28:25 +0200

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,16 +1,16 @@
 const wordsList = new Array();
 function addWords() {
-  let add = document.getElementById("words").value;
+  let add = document.getElementById("added-word").value;
   wordsList.push(add);  
 }
 
 function checkWord() {
-  let check = document.getElementById("check").value;
+  let check = document.getElementById("searchedWord").value;
   if (wordsList.includes(check)) {
-    document.getElementById("displayResult").style.color = "green";
-    document.getElementById("displayResult").innerHTML = "This word there is in the dictionary!";
+    document.getElementById("dictionary-query-message").style.color = "green";
+    document.getElementById("dictionary-query-message").innerHTML = "This word there is in the dictionary!";
   } else {
-    document.getElementById("displayResult").style.color = "red";
-    document.getElementById("displayResult").innerHTML = "This word there is not in the dictionary!";
+    document.getElementById("dictionary-query-message").style.color = "red";
+    document.getElementById("dictionary-query-message").innerHTML = "This word there is not in the dictionary!";
   }
 }
